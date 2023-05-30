a1f07afd1524a255617d32b871343ac2577f7f1f
Wisdom Ochei <wisdomofgod2003@gmail.com>
2023-05-30 18:51:26 +0100

    Corrected animation error

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,7 +17,7 @@ const Nav = () => {
           <Line
             transition={{ duration: 0.75 }}
             initial={{ width: "0%" }}
-            animate={{ width: pathname === "/" ? "50%" : "0%" }}
+            animate={{ width: pathname === "/aboutus" ? "50%" : "0%" }}
           />
         </li>
         <li>
